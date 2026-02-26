2b1c9590851220841ea287ddd50218fb78107d73
ThelmaMattsson <Thelma.mattsson@hotmail.com>
2026-02-26 15:31:54 +0100

    made cards bigger on mobile screen

--- src/components/GlosSearch.tsx
+++ src/components/GlosSearch.tsx
@@ -1,28 +1,51 @@
 type GlosSearchProps = {
-    value: string;
-    onChange: (value: string) => void;
-    course: string;
-    onCourseChange: (value: string) => void;
+    value: string
+    onChange: (value: string) => void
+    course: string
+    onCourseChange: (value: string) => void
 }
 
-export function GlosSearch({ value, onChange, course, onCourseChange }: GlosSearchProps) {
+export function GlosSearch({
+    value,
+    onChange,
+    course,
+    onCourseChange,
+}: GlosSearchProps) {
     return (
         <div>
             <div>
-                <label className="block text-sm text-text-inverted font-medium mb-1">Filtrera kurs</label>
+                <label
+                    htmlFor="course-filter"
+                    className="block text-sm text-text-inverted font-medium mb-1"
+                >
+                    Filtrera kurs
+                </label>
                 <select
-                value={course}
-                onChange={(e) => onCourseChange(e.target.value)}
-                className="border-1 border-gray-400 p-2 rounded-md text-text-inverted cursor-pointer"
-            >
-                <option value="All">Visa alla</option>
-                <option value="UX">UX</option>
-                <option value="Agilt arbete">Agilt arbete</option>
-                <option value="apputveckling">Apputveckling</option>
-            </select>
+                    id="course-filter"
+                    value={course}
+                    onChange={(e) => onCourseChange(e.target.value)}
+                    className="border-1 border-gray-400 p-2 rounded-md text-text-inverted cursor-pointer"
+                >
+                    <option value="All">Visa alla</option>
+                    <option value="UX">UX</option>
+                    <option value="Agilt arbete">Agilt arbete</option>
+                    <option value="apputveckling">Apputveckling</option>
+                </select>
             </div>
-            <label htmlFor="glos-search" className="block text-sm font-medium text-text-inverted mb-1">Sök</label>
-            <input value={value} onChange={(e) => onChange(e.target.value)} type="text" placeholder="Sök efter glosa..." className="border-1 border-gray-400 p-2 text-gray-700 rounded-md w-full"/>
+            <label
+                htmlFor="searchglosa"
+                className="block text-sm font-medium text-text-inverted mb-1"
+            >
+                Sök
+                <input
+                    id="searchglosa"
+                    value={value}
+                    onChange={(e) => onChange(e.target.value)}
+                    type="text"
+                    placeholder="Sök efter glosa..."
+                    className="border-1 border-gray-400 p-2 text-gray-700 rounded-md w-full"
+                />
+            </label>
         </div>
     )
 }
